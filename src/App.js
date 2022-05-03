d6ccadb51a5f883d0ed5b5032dcd530a34eaf71c
강승훈 <iltk2000@gmail.com>
2022-05-03 20:39:05 +0900

    vercel change event

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn github actio test!~
+          vercel change
         </a>
       </header>
     </div>
