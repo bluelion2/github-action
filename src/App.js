a8a8be346913e694b06e7d5a23cd041301d788c1
강승훈 <iltk2000@gmail.com>
2022-05-03 13:00:20 +0900

    text fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn github action2
+          Learn github actio test!~
         </a>
       </header>
     </div>
