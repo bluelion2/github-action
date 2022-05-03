5809153b954065c08791c6598d2e2fab74b81862
강승훈 <iltk2000@gmail.com>
2022-05-03 20:36:59 +0900

    fix js

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  console.log('core', core)
-  console.log('github', github)
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const changedComment = payload.comment.body
+  const prBody = payload.issue.body
+
+  console.log('changedComment', changedComment)
+  console.log('prBody', prBody)
+
 } catch (error) {
   core.setFailed(error.message);
 }
