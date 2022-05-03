36f3a91c07ff1dd8c3a2747e47a67086cf838af2
강승훈 <iltk2000@gmail.com>
2022-05-03 20:40:59 +0900

    minor fix

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const github = require('@actions/github');
 try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const changedComment = payload.comment.body
-  const prBody = payload.issue.body
+
+  console.log("typeof payload", typeof payload)
+  const changedComment = github.context.payload.comment.body
+  const prBody = github.context.payload.issue.body
 
   console.log('changedComment', changedComment)
   console.log('prBody', prBody)
