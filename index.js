297c360be3296a30ac1ce2c90a3d03a30afd6a5a
강승훈 <iltk2000@gmail.com>
2022-05-04 12:37:44 +0900

    fix js

--- index.js
+++ index.js
@@ -43,13 +43,24 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
-  console.log("typeof payload", typeof payload)
+  // console.log("typeof payload", typeof payload)
   const changedComment = github.context.payload.comment.body
   const prBody = github.context.payload.issue.body
 
-  console.log('changedComment', changedComment)
-  console.log('prBody', prBody)
-  action.post(`/issue/${issueKey}/comment`, { body })
+  // console.log('changedComment', changedComment)
+  // console.log('prBody', prBody)
+  fetch(`https://freewheelin.atlassian.net/rest/api/3/issue/${issueKey}/comment`, {
+    headers: {
+      'Authorization': `Basic ${Buffer.from(
+        'ksh0228@mathflat.com:G46Tsw7PCPTgv9OJUGmM8AB5'
+      ).toString('base64')}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body
+  })
+
+  // action.post(`/issue/${issueKey}/comment`, { body })
 
 } catch (error) {
   core.setFailed(error.message);
