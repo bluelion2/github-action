0dff9894cd3b7ca3b714fc27daa50f77dca22631
강승훈 <iltk2000@gmail.com>
2022-05-04 18:23:03 +0900

    fix

--- index.js
+++ index.js
@@ -4,33 +4,10 @@ const axios = require('axios')
 
 const issueKey =  'MATH2SP-431'
 
-const body = `{
-  "visibility": {
-    "identifier": "Administrators",
-    "type": "role",
-    "value": "Administrators"
-  },
-  "body": {
-    "type": "doc",
-    "version": 1,
-    "content": [
-      {
-        "type": "paragraph",
-        "content": [
-          {
-            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper.",
-            "type": "text"
-          }
-        ]
-      }
-    ]
-  }
-}`;
-
 
 
 try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
 
   // console.log("typeof payload", typeof payload)
@@ -49,23 +26,59 @@ try {
   //   },
   //   body
   // })
-  const auth = `Basic ${Buffer.from(
-    'ksh0228@mathflat.com:ieyv6sldxxY1D8Z81UEaAB12'
-  ).toString('base64')}`
 
-  console.log('auth', auth)
+  // preview url
+  // /review.+(https?\S+\.vercel\.app)/
+  // jira url
+  // /(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/
+  const [, vercelUrl] = changedComment.match(/review.+(https?\S+\.vercel\.app)/)
 
-  const action = axios.create({
-    baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
-    headers: {
-      'Authorization': auth,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  })
+  const [, jiraUrl,issueKey] = prBody.match(/(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/)[0]
+  console.log('jiraUrl', jiraUrl)
+  console.log('vercelUrl', vercelUrl)
+  if (vercelUrl && jiraUrl) {
+    
+    const auth = `Basic ${Buffer.from(
+      'ksh0228@mathflat.com:ieyv6sldxxY1D8Z81UEaAB12'
+    ).toString('base64')}`
 
-  action.post(`/issue/${issueKey}/comment`, body)
+    console.log('auth', auth)
+    const body = `{
+      "visibility": {
+        "identifier": "Administrators",
+        "type": "role",
+        "value": "Administrators"
+      },
+      "body": {
+        "type": "doc",
+        "version": 1,
+        "content": [
+          {
+            "type": "paragraph",
+            "content": [
+              {
+                "text": "preview url : ${vercelUrl}"
+                "type": "text"
+              }
+            ]
+          }
+        ]
+      }
+    }`;
 
+    console.log('body', body)
+
+    const action = axios.create({
+      baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
+      headers: {
+        'Authorization': auth,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+
+    action.post(`/issue/${issueKey}/comment`, body)
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
