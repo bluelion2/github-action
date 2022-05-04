d30f9df34634bebbc2ef29d1a5bd980aa2bd6424
강승훈 <iltk2000@gmail.com>
2022-05-04 12:08:14 +0900

    add buffer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios')
+const Buffer = require('buffer')
+
 
 const action = axios.create({
   baseUrl: 'https://freewheelin.atlassian.net/rest/api/3',
