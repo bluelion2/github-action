58788dc0534809932218b4648c576f2d999252c2
강승훈 <iltk2000@gmail.com>
2022-05-06 14:33:20 +0900

    secret token

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
   // console.log("typeof payload", typeof payload)
   const changedComment = github.context.payload.comment.body
   const prBody = github.context.payload.issue.body
+  console.log('github', github)
 
   console.log('changedComment', changedComment)
   console.log('prBody', prBody)
@@ -27,48 +28,48 @@ try {
   // /review.+(https?\S+\.vercel\.app)/
   // jira url
   // /(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/
-  const [, vercelUrl] = changedComment.match(/review.+(https?\S+\.vercel\.app)/)
+  // const [, vercelUrl] = changedComment.match(/review.+(https?\S+\.vercel\.app)/)
 
-  const [, jiraUrl,issueKey] = prBody.match(/(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/)
-  console.log('jiraUrl', jiraUrl)
-  console.log('vercelUrl', vercelUrl)
-  if (vercelUrl && jiraUrl) {
+  // const [, jiraUrl,issueKey] = prBody.match(/(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/)
+  // console.log('jiraUrl', jiraUrl)
+  // console.log('vercelUrl', vercelUrl)
+  // if (vercelUrl && jiraUrl) {
     
-    const auth = `Basic ${Buffer.from(
-      'ksh0228@mathflat.com:U0TROZwMjSObtWMSP7Qk67FD'
-    ).toString('base64')}`
+  //   const auth = `Basic ${Buffer.from(
+  //     'ksh0228@mathflat.com:U0TROZwMjSObtWMSP7Qk67FD'
+  //   ).toString('base64')}`
 
-    console.log('auth', auth)
-    const body = `{
-      "body": {
-        "type": "doc",
-        "version": 1,
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "text": "preview url : ${vercelUrl}",
-                "type": "text"
-              }
-            ]
-          }
-        ]
-      }
-    }`;
+  //   console.log('auth', auth)
+  //   const body = `{
+  //     "body": {
+  //       "type": "doc",
+  //       "version": 1,
+  //       "content": [
+  //         {
+  //           "type": "paragraph",
+  //           "content": [
+  //             {
+  //               "text": "preview url : ${vercelUrl}",
+  //               "type": "text"
+  //             }
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   }`;
 
-    console.log('body', body)
+  //   console.log('body', body)
 
-    const action = axios.create({
-      baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
-      headers: {
-        'Authorization': auth,
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    })
+  //   const action = axios.create({
+  //     baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
+  //     headers: {
+  //       'Authorization': auth,
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //   })
 
-    action.post(`/issue/${issueKey}/comment`, body)
+  //   action.post(`/issue/${issueKey}/comment`, body)
   }
 } catch (error) {
   core.setFailed(error.message);
