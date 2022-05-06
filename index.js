339fa07b1d688e0db6eb308f69644e8c2f56a063
강승훈 <iltk2000@gmail.com>
2022-05-06 15:15:18 +0900

    코드 정리 완료

--- index.js
+++ index.js
@@ -3,76 +3,60 @@ const github = require('@actions/github');
 const axios = require('axios')
 
 try {
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-
-  // console.log("typeof payload", typeof payload)
   const changedComment = github.context.payload.comment.body
   const prBody = github.context.payload.issue.body
-  // console.log('github', github)
-
-  // console.log('changedComment', changedComment)
-  // console.log('prBody', prBody)
   const jiraToken = core.getInput('jira-token')
-  // console.log("jira", jiraToken)
-  // fetch(`https://freewheelin.atlassian.net/rest/api/3/issue/${issueKey}/comment`, {
-  //   headers: {
-  //     'Authorization': `Basic ${Buffer.from(
-  //       'ksh0228@mathflat.com:G46Tsw7PCPTgv9OJUGmM8AB5'
-  //     ).toString('base64')}`,
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body
-  // })
 
   // preview url
   // /review.+(https?\S+\.vercel\.app)/
   // jira url
   // /(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/
-  const [, vercelUrl] = changedComment.match(/review.+(https?\S+\.vercel\.app)/)
+  
+  const vercelRegExp = changedComment.match(/review.+(https?\S+\.vercel\.app)/)
+  const jiraRegExp = prBody.match(/(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/)
+  
+  if (vercelRegExp && jiraRegExp) {
+    const [,vercelUrl] = vercelRegExp
+    const [, jiraUrl,issueKey] = jiraRegExp
+  
+    if (vercelUrl && jiraUrl) {
+      
+      const auth = `Basic ${Buffer.from(
+        `ksh0228@mathflat.com:${jiraToken}`
+      ).toString('base64')}`
+  
+      console.log('auth', auth)
+      const body = `{
+        "body": {
+          "type": "doc",
+          "version": 1,
+          "content": [
+            {
+              "type": "paragraph",
+              "content": [
+                {
+                  "text": "preview url : ${vercelUrl}",
+                  "type": "text"
+                }
+              ]
+            }
+          ]
+        }
+      }`;
 
-  const [, jiraUrl,issueKey] = prBody.match(/(https?:\/\/freewheelin\.atlassian\.net\/browse\/(\S+\-[0-9]+))/)
-  console.log('jiraUrl', jiraUrl)
-  console.log('vercelUrl', vercelUrl)
-  if (vercelUrl && jiraUrl) {
-    
-    const auth = `Basic ${Buffer.from(
-      `ksh0228@mathflat.com:${jiraToken}`
-    ).toString('base64')}`
-
-    console.log('auth', auth)
-    const body = `{
-      "body": {
-        "type": "doc",
-        "version": 1,
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "text": "preview url : ${vercelUrl}",
-                "type": "text"
-              }
-            ]
-          }
-        ]
-      }
-    }`;
-
-    console.log('body', body)
-
-    const action = axios.create({
-      baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
-      headers: {
-        'Authorization': auth,
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    })
-
-    action.post(`/issue/${issueKey}/comment`, body)
+      const action = axios.create({
+        baseURL: 'https://freewheelin.atlassian.net/rest/api/3',
+        headers: {
+          'Authorization': auth,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      })
+  
+      action.post(`/issue/${issueKey}/comment`, body)
+    }
   }
+ 
 } catch (error) {
   core.setFailed(error.message);
 }
