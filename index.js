9fb4799d620f29c5d83a5232eb4a1c5e8f579414
강승훈 <iltk2000@gmail.com>
2022-05-09 17:51:54 +0900

    fix

--- index.js
+++ index.js
@@ -37,19 +37,19 @@ try {
                 {
                   "text": "preview url : ${vercelUrl}",
                   "type": "text"
-                }
-              ]
-            },
-            {
-              "type": "text",
-              "text": "Hello world",
-              "marks": [
+                },
                 {
-                  "type": "link",
-                  "attrs": {
-                    "href": "http://atlassian.com",
-                    "title": "Atlassian"
-                  }
+                  "type": "text",
+                  "text": "Hello world",
+                  "marks": [
+                    {
+                      "type": "link",
+                      "attrs": {
+                        "href": "http://atlassian.com",
+                        "title": "Atlassian"
+                      }
+                    }
+                  ]
                 }
               ]
             }
